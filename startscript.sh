a0eb562e854d7d82fae220f2ff587cc97eb773ee
buschco <colin@busch.dev>
2020-01-06 17:54:19 +0100

    Use functions

--- startscript.sh
+++ startscript.sh
@@ -4,34 +4,37 @@
 # so hacky...
 sleep 20
 # send ip and port to circleci
+
+function post_ip() {
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
+        -d $@
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
+        -d $@
+}
+
+function post_port() {
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
+        -d $@
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
+        -d $@
+}
+
 curl -s http://localhost:4040/api/tunnels |
 jq -r .tunnels[0].public_url |
 sed -e 's/tcp:\/\/*//; s/:.*//; s/^/name=PI_IP\nvalue=/' |
 jo |
-tee \
-# homebridge-config
->(curl -H "Content-Type: application/json" \
--X POST \
---url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
--d @-) \
-# latex
->(curl -H "Content-Type: application/json" \
--X POST \
---url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
--d @-)
+post_ip $@
 
 curl -s http://localhost:4040/api/tunnels |
 jq -r .tunnels[0].public_url |
 sed -e 's/tcp:\/\/*//; s/.*://; s/^/name=PI_PORT\nvalue=/' |
 jo |
-tee \
-# homebridge-config
->(curl -H "Content-Type: application/json" \
--X POST \
---url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
--d @-)
-# latex
->(curl -H "Content-Type: application/json" \
--X POST \
---url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
--d @-)
+post_port $@
