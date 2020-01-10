7a4254614e50a43d0075b5056a0ce2d189eccd53
buschco <colin@busch.dev>
2020-01-10 16:59:31 +0100

    Added config and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# ngrok-address-update-script
+
+## Setup
+1. Download and install [ngrok](https://ngrok.com/download)
+2. Clone the repo and move it to a nice place
+3. Edit [urls.sample.txt](urls.sample.txt) and rename it to `urls.txt`
+4. Make sure the scripts will be executed at system start.
+```sh
+chmod +x ngrok 
+chmod +x startscript.s
+crontab -e
+#paste
+#@reboot /path/to/startscript.sh 1> /path/to/start.log 2> /path/to/start.err.log
+```
--- startscript.sh
+++ startscript.sh
@@ -1,40 +1,29 @@
 #!/bin/sh
 # start ngrok
+killall ngrok
 ./ngrok tcp 22 --log=stdout > ngrok.log &
 # so hacky...
 sleep 20
-# send ip and port to circleci
-
-function post_ip() {
-    curl -H "Content-Type: application/json" \
-        -X POST \
-        --url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
-        -d $@
-    curl -H "Content-Type: application/json" \
-        -X POST \
-        --url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
-        -d $@
-}
-
-function post_port() {
-    curl -H "Content-Type: application/json" \
-        -X POST \
-        --url https://circleci.com/api/v1.1/project/github/buschco/homebridge-config/envvar?circle-token=$CIRCLECI_TOKEN \
-        -d $@
-    curl -H "Content-Type: application/json" \
-        -X POST \
-        --url https://circleci.com/api/v1.1/project/github/buschco/latex/envvar?circle-token=$CIRCLECI_TOKEN \
-        -d $@
-}
-
-curl -s http://localhost:4040/api/tunnels |
+# send ip and port to urls
+IP=$(curl -s http://localhost:4040/api/tunnels |
 jq -r .tunnels[0].public_url |
 sed -e 's/tcp:\/\/*//; s/:.*//; s/^/name=PI_IP\nvalue=/' |
-jo |
-post_ip $@
+jo)
 
-curl -s http://localhost:4040/api/tunnels |
+PORT=$(curl -s http://localhost:4040/api/tunnels |
 jq -r .tunnels[0].public_url |
 sed -e 's/tcp:\/\/*//; s/.*://; s/^/name=PI_PORT\nvalue=/' |
-jo |
-post_port $@
+jo)
+
+while read url; do
+    echo "Posting IP: $IP to $url"
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url $url \
+        -d $IP
+    echo "Posting PORT: $PORT to $url"
+    curl -H "Content-Type: application/json" \
+        -X POST \
+        --url $url \
+        -d $PORT
+done <urls.txt
